fe91b0644c1135a33fb6662c0f76b82649a02960
Corinna Brock <corinna.brock@gmail.com>
2012-06-20 20:12:09 -0400

    first commit

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,6 @@
+class HelloController < ApplicationController
+  def index
+    
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '2ed3905f24e79bf433ccc3ceb158ffd4d0758b9227d84d88cf3fcf7d11836d4569b2eae77718a19255293e90f26ea9c388264b8ec1675961d1ec43286aee9fc6'
